9ed2a32445a1de7c28f2945441fad3fc74e4ba81
zhf-pythoner <zhaohongfei110@gmail.com>
2016-10-25 23:12:16 +0800

    test

---
+++ pickets.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# __Author__: Zhaohongfei
+
+
+"""Train tickets query via command-line.
+
+Usage:
+    tickets [-gdtkz] <from> <to> <date>
+
+Options:
+    -h,--help   显示帮助菜单
+    -g          高铁
+    -d          动车
+    -t          特快
+    -k          快速
+    -z          直达
+
+Example:
+    tickets beijing shanghai 2016-08-25
+"""
+from docopt import docopt
+
+
+def cli():
+    """command-line interface"""
+    arguments = docopt(__doc__)
+    print(arguments)
+
+if __name__ == '__main__':
+    cli()
+import os
